1478794eb4e2139ea287d6fa7903428a83fbb7a2
Jehyeok Lee <jehyeoklee@Jehyeokui-MacBook-Pro.local>
2020-01-05 21:16:11 +0900

    add isSymmetric_iteration solution

---
+++ leetcode/symmetricTree.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <stack>
+#include <queue>
+#include "../utils.h"
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode (int v)
+    : val(v)
+    , left(nullptr)
+    , right(nullptr)
+    {
+        
+    }
+    TreeNode (vector<int> vals)
+    : val(-1)
+    , left(nullptr)
+    , right(nullptr)
+    {
+        auto h = false;
+        auto l = false;
+        queue<TreeNode*> q;
+        for (auto v : vals) {
+            
+            if (!h)
+            {
+                val = v;
+                h = true;
+                q.push(this);
+            }
+            else
+            {
+                auto nxt = (v != -1) ? new TreeNode(v) : nullptr;
+                l = !l;
+                
+                auto cur = q.front();
+                if (!l)
+                {
+                    if (cur != nullptr)
+                        cur->right = nxt;
+                    q.pop();
+                }
+                else
+                {
+                    if (cur != nullptr)
+                        cur->left = nxt;
+                }
+                
+                q.push(nxt);
+            }
+        }
+    }
+
+    bool operator==(const TreeNode& r) const
+    {
+        auto result = val == r.val;
+        result &= (left != nullptr) ? r.left != nullptr : r.left == nullptr;
+        result &= (right != nullptr) ? r.right != nullptr : r.right == nullptr;
+        if (result)
+        {
+            if (left != nullptr)
+            {
+                result &= *left == *r.left;
+            }
+            if (right != nullptr)
+            {
+                result &= *right == *r.right;
+            }
+        }
+        return result;
+    }
+};
+
+
+//       1
+//   2      2
+// 3   4  4   3
+
+//       1
+//   2      2
+//     3       3
+
+//          1
+//     2          2
+//  3     4    4     3
+//5   6            6   5
+
+bool isSymmetric_iteration(TreeNode* root) {
+    if (root == nullptr) return true;
+    
+    auto l = root;
+    auto r = root;
+
+    stack<TreeNode*> ls;
+    stack<TreeNode*> rs;
+    while (!ls.empty() || l != nullptr) {
+        if ((l != nullptr && r == nullptr) || (l == nullptr && r != nullptr)) return false;
+        
+        if (l != nullptr)
+        {
+            ls.push(l);
+            rs.push(r);
+            l = l->left;
+            r = r->right;
+        }
+        else
+        {
+            l = ls.top();
+            r = rs.top();
+            ls.pop();
+            rs.pop();
+
+            if (l->val != r->val) return false;
+
+            l = l->right;
+            r = r->left;
+        }
+    }
+
+    return true;
+}
+
+bool isSymmetric_recursion(TreeNode* s, TreeNode* t) {
+    if (s == nullptr && t == nullptr) return true;
+    if (s == nullptr || t == nullptr) return false;
+
+    auto ans = s->val == t->val;
+    ans &= isSymmetric_recursion(s->left, t->right); 
+    ans &= isSymmetric_recursion(s->right, t->left);
+
+    return ans;
+}
+
+bool isSymmetric(TreeNode* root) {
+    return isSymmetric_iteration(root);
+    //return isSymmetric_recursion(root, root);
+}
+
+int main()
+{
+    assert(isSymmetric(
+        new TreeNode(vector<int>{1,2,2,3,4,4,3}))
+        == true);
+    assert(isSymmetric(
+        new TreeNode(vector<int>{1,2,2,-1,3,-1,3}))
+        == false);
+    return 0;
+}
