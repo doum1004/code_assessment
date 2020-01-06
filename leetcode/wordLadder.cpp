22611c727f537a94060edd9b460842444290584a
Jehyeok Lee <jehyeoklee@Jehyeokui-MacBook-Pro.local>
2020-01-06 20:09:22 +0900

    sikve wordLadder with bidirectional BFS

--- leetcode/wordLadder.cpp
+++ leetcode/wordLadder.cpp
@@ -102,67 +102,136 @@ struct TreeNode {
 // Output: 0
 
 // Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
-
-int ladderLength_singleDirectional(string beginWord, string endWord, vector<string>& wordList) {
-    auto n = beginWord.size();
-    auto transformedWrodsMap = unordered_map<string, vector<string>>();
-    for (auto &word : wordList)
-    {
-        for (int i=0; i<n; i++)
-        {
-            auto new_word = word.substr(0, i) + "*" + word.substr(i+1, n);
-            vector<string> transformedWrods;
-            if (transformedWrodsMap.find(new_word) != transformedWrodsMap.end()) {
-                transformedWrods = transformedWrodsMap[new_word];
-            }
-            transformedWrods.push_back(word);
-            transformedWrodsMap[new_word] = transformedWrods;
-        }
-    }
-    
-    auto q = queue<pair<string, int>>();
-    q.push(pair(beginWord, 1));
-
-    auto visited = unordered_set<string>();
-    visited.insert(beginWord); // ?
-    
-    while (!q.empty()) {
+class Solution {
+    public:
+    int visitWord(const int n, unordered_map<string, vector<string>>& transformedWords, queue<pair<string, int>>& q, unordered_map<string, int>& visited, unordered_map<string, int>& othervisited) {
         auto word = q.front().first;
         auto level = q.front().second;
         q.pop();
-        
-        for (int i=0; i<n; i++)
-        {
-            auto new_word = word.substr(0,i) + "*" + word.substr(i+1, n);
-            if (transformedWrodsMap.find(new_word) != transformedWrodsMap.end()) {
-                for (auto &found : transformedWrodsMap[new_word]) {
-                    if (found == endWord) {
-                        return level + 1;
+
+        for (int i=0; i<n; i++) {
+            auto new_word = word.substr(0, i) + "*" + word.substr(i+1, n);
+            if (transformedWords.find(new_word) != transformedWords.end()) {
+                for (auto found : transformedWords[new_word]) {
+                    if (othervisited.find(found) != othervisited.end()) {
+                        return level + othervisited[found];
                     }
-                    
+
                     if (visited.find(found) == visited.end()) {
-                        visited.insert(found);
+                        visited[found] = level + 1;
                         q.push(pair(found, level + 1));
                     }
                 }
             }
         }
+
+        return -1;
     }
 
-    return 0;
-}
+    int ladderLength_bidirectional(string beginWord, string endWord, vector<string>& wordList) {
+        if (find(wordList.begin(), wordList.end(), endWord) == wordList.end()) return 0;
 
-int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-    return ladderLength_singleDirectional(beginWord, endWord, wordList);
-}
+        // map for transformed words
+        auto n = beginWord.size();
+        auto transformedWords = unordered_map<string, vector<string>>();
+        for (auto &word : wordList) {
+            for (int i=0; i<n; i++) {
+                auto new_word = word.substr(0, i) + "*" + word.substr(i+1, n);
+                vector<string> words;
+                if (transformedWords.find(new_word) != transformedWords.end()) {
+                    words = transformedWords[new_word];
+                }
+                words.push_back(word);
+                transformedWords[new_word] = words;
+            }
+        }
+
+        // queue for BFS
+        auto b_q = queue<pair<string, int>>();
+        b_q.push(pair(beginWord, 1));
+        auto e_q = queue<pair<string, int>>();
+        e_q.push(pair(endWord, 1));
+        
+        // set for visited
+        auto b_visited = unordered_map<string, int>();
+        b_visited[beginWord] = 1;
+        auto e_visited = unordered_map<string, int>();
+        e_visited[endWord] = 1;
+
+        while (!b_q.empty() && !e_q.empty()) {
+            auto ans1 = visitWord(n, transformedWords, b_q, b_visited, e_visited);
+            if (ans1 > -1)
+                return ans1;
+            auto ans2 = visitWord(n, transformedWords, e_q, e_visited, b_visited);
+            if (ans2 > -1)
+                return ans2;
+        }
+
+        return 0;
+    }
+
+    int ladderLength_singledirectional(string beginWord, string endWord, vector<string>& wordList) {
+        auto n = beginWord.size();
+        auto transformedWrodsMap = unordered_map<string, vector<string>>();
+        for (auto &word : wordList)
+        {
+            for (int i=0; i<n; i++)
+            {
+                auto new_word = word.substr(0, i) + "*" + word.substr(i+1, n);
+                vector<string> transformedWrods;
+                if (transformedWrodsMap.find(new_word) != transformedWrodsMap.end()) {
+                    transformedWrods = transformedWrodsMap[new_word];
+                }
+                transformedWrods.push_back(word);
+                transformedWrodsMap[new_word] = transformedWrods;
+            }
+        }
+        
+        auto q = queue<pair<string, int>>();
+        q.push(pair(beginWord, 1));
+
+        auto visited = unordered_set<string>();
+        visited.insert(beginWord); // ?
+        
+        while (!q.empty()) {
+            auto word = q.front().first;
+            auto level = q.front().second;
+            q.pop();
+            
+            for (int i=0; i<n; i++)
+            {
+                auto new_word = word.substr(0,i) + "*" + word.substr(i+1, n);
+                if (transformedWrodsMap.find(new_word) != transformedWrodsMap.end()) {
+                    for (auto &found : transformedWrodsMap[new_word]) {
+                        if (found == endWord) {
+                            return level + 1;
+                        }
+                        
+                        if (visited.find(found) == visited.end()) {
+                            visited.insert(found);
+                            q.push(pair(found, level + 1));
+                        }
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        return ladderLength_bidirectional(beginWord, endWord, wordList);
+        //return ladderLength_singledirectional(beginWord, endWord, wordList);
+    }
+};
 
 int main()
 {
     auto input1 = vector<string> {"hot","dot","dog","lot","log","cog"};
-    assert(ladderLength("hit", "cog", input1)
+    assert(Solution().ladderLength("hit", "cog", input1)
         == 5);
     auto input2 = vector<string> {"hot","dot","dog","lot","log"};
-    assert(ladderLength("hit", "cog", input2)
+    assert(Solution().ladderLength("hit", "cog", input2)
         == 0);
     return 0;
 }
