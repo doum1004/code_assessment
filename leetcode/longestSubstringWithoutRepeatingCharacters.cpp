aa4414c8d4848d1a2a1a7bd773b5579790a5c3f5
doum1004 <doum1004@gmail.com>
2020-01-14 20:54:41 +0900

    update lengthOfLongestSubstring

--- leetcode/longestSubstringWithoutRepeatingCharacters.cpp
+++ leetcode/longestSubstringWithoutRepeatingCharacters.cpp
@@ -5,95 +5,40 @@
 
 using namespace std;
 
-int lengthOfLongestSubstring2(string s) {
-    auto n = s.size();
-    if (n < 1) return 0;
-    
-    auto answer = 0;
-    auto map = unordered_map<char, int>();
-    for (int i=0, j=0; j<n; j++)
-    {
-        if (map.find(s.at(j)) != map.end())
-        {
-            i = max(i, map[s.at(j)]);           
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // sliding door (it can be o(n) sliding door)
+        // time complexcity: o(n) * 2 = o(n)
+        // space complexcity: o(k<n)
+        auto n = s.size();
+        if (n < 2) return n;
+
+        auto cache = unordered_set<char>();
+        int i = 0, j = 0;
+        int ans = 0;
+        while (i < n && j < n) {
+            if (cache.find(s[j]) == cache.end()) {
+                cache.insert(s[j++]);
+                ans = max(ans, j-i);
+            }
+            else {
+                cache.erase(s[i++]);
+            }
         }
-        answer = max(answer, j-i + 1);
-        map[s.at(j)] = j + 1;
+
+        return ans;
     }
-
-    return answer; 
-}
-
-int lengthOfLongestSubstring3(string s) {
-    auto n = s.size();
-    if (n < 1) return 0;
-    
-    auto set = unordered_set<char>();
-    
-    auto answer = 0, i = 0, j = 0;
-    while (i < n && j < n)
-    {
-        if (set.find(s.at(j)) == set.end())
-        {
-            set.insert(s.at(j++));
-            answer = max(answer, (int)set.size());
-        }
-        else
-        {
-            set.erase(s.at(i++));
-        }
-    }
-
-    return answer; 
-}
-
-
-int lengthOfLongestSubstring(string s) {
-    if (s.size() < 1) return 0;
-
-    auto answer = 1;
-    
-    auto list = vector<char>();
-    for (int i=0, j=0; i<s.size(); i++)
-    {
-        auto it = find(list.begin(), list.end(), s.at(i));
-        if (it != list.end() && it - list.begin() >= j++)
-        {
-            j++;
-        }
-        list.push_back(s.at(i));
-        answer = max(answer, i - j + 1);
-    }
-
-    return answer; 
-}
-
-int lengthOfLongestSubstring0(string s) {
-    if (s.size() < 1) return 0;
-
-    auto answer = 1;
-    
-    auto list = vector<char>();
-    for (int i=0; i<s.size(); i++)
-    {
-        auto it = find(list.begin(), list.end(), s.at(i));
-        if (it != list.end())
-        {
-            list.erase(list.begin(), it+1);
-        }
-        list.push_back(s.at(i));
-        answer = max(answer, (int)list.size());
-    }
-
-    return answer; 
-}
+};
 
 int main()
 {
-    assert(lengthOfLongestSubstring("abcabcbb") == 3);
-    assert(lengthOfLongestSubstring("aab") == 2);
-    assert(lengthOfLongestSubstring("pwwkew") == 3);
-    assert(lengthOfLongestSubstring("ohvhjdml") == 6);
-    assert(lengthOfLongestSubstring("abba") == 2);
+    assert(Solution().lengthOfLongestSubstring("abcabcbb") == 3);
+    assert(Solution().lengthOfLongestSubstring("aab") == 2);
+    assert(Solution().lengthOfLongestSubstring("pwwkew") == 3);
+    assert(Solution().lengthOfLongestSubstring("ohvhjdml") == 6);
+    assert(Solution().lengthOfLongestSubstring("abba") == 2);
     return 0;
 }
