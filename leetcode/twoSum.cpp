8bcb213a2f0a722a25cca20f43da9f5e6d8d55d0
doum1004 <doum1004@gmail.com>
2020-01-14 20:35:54 +0900

    update two-sum

--- leetcode/twoSum.cpp
+++ leetcode/twoSum.cpp
@@ -5,48 +5,43 @@
 
 using namespace std;
 
-vector<int> twoSum(vector<int>& nums, int target) {
-    auto n = nums.size();
-    auto map = unordered_map<int, int>(n);
-    for (int i=0; i<n; i++) {
-        map[nums[i]] = i;
-    }
-    
-    vector<int> answer(0);
-    for (int i=0; i<n; i++)
-    {
-        auto complement = target - nums[i];
-        if (map.find(complement) != map.end() && map[complement] != i)
-            return vector<int>{i, map[complement]};
-    }
-    throw new exception();
-    //vector<int> answer(0);
-    // for (int i = 0; i<nums.size() - 1; i++)
-    // {
-    //     for (int j = i+1; j<nums.size(); j++)
-    //     {
-    //         if (nums[i] + nums[j] == target)
-    //         {
-    //             answer.push_back(i);
-    //             answer.push_back(j);
-    //             break;
-    //         }
-    //     }
-    // }
+// https://leetcode.com/problems/two-sum/
 
-    //return answer;
-}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //two path hash table (it can be one path)
+        //time complexcity: o(n) + (o(n) * o(1)) -> o(n)
+        //space complexcityL o(n)
+
+        if (nums.size() < 2) return vector<int>();
+
+        auto diff = unordered_map<int, int>();
+        for (int i=0; i<nums.size(); ++i) {
+            diff[target - nums[i]] = i;
+        }
+        
+        for (int i=0; i<nums.size(); ++i) {
+            auto num = nums[i];
+            if (diff.find(num) != diff.end() && i != diff[num]) {
+                return vector<int> {i, diff[num]};
+            }
+        }
+
+        return vector<int>();
+    }
+};
 
 int main()
 {
     auto input1 = vector<int>{2, 7, 11, 15};
     auto input2 = vector<int>{3, 2, 4};
     auto input3 = vector<int>{-3,4,3,90};
-    assert(twoSum(input1, 9) == (vector<int>{0, 1}));
-    assert(twoSum(input1, 13) == (vector<int>{0, 2}));
-    assert(twoSum(input1, 18) == (vector<int>{1, 2}));
-    assert(twoSum(input2, 6) == (vector<int>{1, 2}));
-    assert(twoSum(input2, 6) == (vector<int>{1, 2}));
-    assert(twoSum(input3, 0) == (vector<int>{0, 2}));
+    assert(Solution().twoSum(input1, 9) == (vector<int>{0, 1}));
+    assert(Solution().twoSum(input1, 13) == (vector<int>{0, 2}));
+    assert(Solution().twoSum(input1, 18) == (vector<int>{1, 2}));
+    assert(Solution().twoSum(input2, 6) == (vector<int>{1, 2}));
+    assert(Solution().twoSum(input2, 6) == (vector<int>{1, 2}));
+    assert(Solution().twoSum(input3, 0) == (vector<int>{0, 2}));
     return 0;
 }
