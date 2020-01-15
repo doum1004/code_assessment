5e07339a0421701a131d6ca08828678638f7acd6
Jehyeok Lee <jehyeok.lee@3dsystems.com>
2020-01-15 17:27:52 +0900

    update integer-to-roman

--- leetcode/integer-to-roman.cpp
+++ leetcode/integer-to-roman.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+// https://leetcode.com/problems/integer-to-roman/
+
 // Symbol       Value
 // I             1
 // V             5
@@ -13,29 +15,31 @@ using namespace std;
 // C             100
 // D             500
 // M             1000
-string intToRoman(int num) {
-    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    string symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    
-    string ans = "";
-    for (int i = 0; i < 13; i++)
-    {
-        while (num - values[i] >= 0)
-        {
-            ans += symbols[i];
-            num -= values[i];
-        }
-    }
 
-    return ans;
-}
+class Solution {
+public:
+    string intToRoman(int num) {
+        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        string ans = "";
+        for (int i=0; i<13; i++) {
+            while (num - values[i] >= 0) {
+                num -= values[i];
+                ans += symbols[i];
+            }
+        }
+        
+        return ans;
+    }
+};
 
 int main()
 {
-    assert(intToRoman(3) == "III");
-    assert(intToRoman(4) == "IV");
-    assert(intToRoman(9) == "IX");
-    assert(intToRoman(58) == "LVIII");
-    assert(intToRoman(1994) == "MCMXCIV");
+    assert(Solution().intToRoman(3) == "III");
+    assert(Solution().intToRoman(4) == "IV");
+    assert(Solution().intToRoman(9) == "IX");
+    assert(Solution().intToRoman(58) == "LVIII");
+    assert(Solution().intToRoman(1994) == "MCMXCIV");
     return 0;
 }
