ca4fa0705e229692930c10b22542c8096d74ff9d
Jehyeok Lee <jehyeok.lee@3dsystems.com>
2020-01-15 17:10:02 +0900

    update roman-to-integer

--- leetcode/roman-to-integer.cpp
+++ leetcode/roman-to-integer.cpp
@@ -1,9 +1,13 @@
+#include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
+#include <unordered_set>
+#include <unordered_map>
 #include <cassert>
-#include "../utils.h"
 
 using namespace std;
+
+// https://leetcode.com/problems/roman-to-integer/
 
 // Symbol       Value
 // I             1
@@ -13,32 +17,38 @@ using namespace std;
 // C             100
 // D             500
 // M             1000
-int romanToInt(string s) {
-    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    string symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-    int ans = 0;
-    auto l = 0;
-    for (int i = 0; i < 13; i++)
-    {
-        auto size = symbols[i].size();
-        while (l < s.size() && symbols[i] == s.substr(l, size))
-        {
-            l += size;
-            ans += values[i];
+class Solution {
+public:
+    int romanToInt(string s) {
+        // arrays for values, symbols
+        // time: o(n)
+        // space: o(1)
+
+        int values[] =      {1000,  900,    500,    400,    100,    90,     50,     40,     10,     9,      5,      4,      1};
+        string symbols[] =  {"M",   "CM",   "D",    "CD",   "C",    "XC",   "L",    "XL",   "X",    "IX",   "V",    "IV",   "I"};
+
+        auto ans = 0;
+        auto j = 0;
+        for (int i=0; i<13; ++i) {
+            auto size = symbols[i].size();
+            while (j < (int)s.size() && symbols[i] == s.substr(j, size)) {
+                j += size;
+                ans += values[i];
+            }
         }
+
+        return ans;
     }
-
-    return ans;
-}
-
+};
+ 
 int main()
 {
-    assert(romanToInt("III") == 3);
-    assert(romanToInt("IV") == 4);
-    assert(romanToInt("IX") == 9);
-    assert(romanToInt("LVIII") == 58);
-    assert(romanToInt("MCMXCIV") == 1994);
-    assert(romanToInt("MMMXLV") == 3045);
-    return 0;
+    assert(Solution().romanToInt("III") == 3); 
+    assert(Solution().romanToInt("IV") == 4); 
+    assert(Solution().romanToInt("IX") == 9);
+    assert(Solution().romanToInt("LVIII") == 58);
+    assert(Solution().romanToInt("MCMXCIV") == 1994);
+    assert(Solution().romanToInt("MMMXLV") == 3045); 
+    return 1;
 }
