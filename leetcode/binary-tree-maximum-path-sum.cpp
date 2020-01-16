fa8f118887ad364ccba7effc6e998c8062080af5
doum1004 <doum1004@gmail.com>
2020-01-16 22:55:08 +0900

    update binary-tree-maximum-path-sum

--- leetcode/binary-tree-maximum-path-sum.cpp
+++ leetcode/binary-tree-maximum-path-sum.cpp
@@ -4,123 +4,64 @@
 #include <stack>
 #include <queue>
 #include "../utils.h"
+#include "../tree.h"
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode (int v)
-    : val(v)
-    , left(nullptr)
-    , right(nullptr)
-    {
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/
+
+//     1(6)
+//2(2)    3(3)
+
+//        -10 (34)
+//  9(9)            20(42, 35)
+//           15(15)      7(7)
+
+// DFS post order
+// Recursive
+// time: o(n)
+// space: o(n)
+
+// ans_max = INT_MIN;
+// int sum(node)
+//   if (node == nullptr) return 0;
+//
+//   l = sum(node->left)
+//   r = sum(node->right)
+//   ans_max = max(ans_max, l + r + node->val)
+//   return node->val + max(l, r)
+// sum(root)
+// return ans;
+
+class Solution {
+private:
+    int ans = INT_MIN;
+
+public:
+    int getMax(TreeNode* root) {
+        if (root == nullptr) return 0;
         
-    }
-    TreeNode (vector<int> vals)
-    : val(-1)
-    , left(nullptr)
-    , right(nullptr)
-    {
-        auto h = false;
-        auto l = false;
-        queue<TreeNode*> q;
-        for (auto v : vals) {
-            
-            if (!h)
-            {
-                val = v;
-                h = true;
-                q.push(this);
-            }
-            else
-            {
-                auto nxt = (v != -1) ? new TreeNode(v) : nullptr;
-                l = !l;
-                
-                auto cur = q.front();
-                if (!l)
-                {
-                    if (cur != nullptr)
-                        cur->right = nxt;
-                    q.pop();
-                }
-                else
-                {
-                    if (cur != nullptr)
-                        cur->left = nxt;
-                }
-                
-                q.push(nxt);
-            }
-        }
+        auto l = max(getMax(root->left), 0);
+        auto r = max(getMax(root->right), 0);
+        ans = max(ans, l + r + root->val);
+
+        return root->val + max(l, r);
     }
 
-    bool operator==(const TreeNode& r) const
-    {
-        auto result = val == r.val;
-        result &= (left != nullptr) ? r.left != nullptr : r.left == nullptr;
-        result &= (right != nullptr) ? r.right != nullptr : r.right == nullptr;
-        if (result)
-        {
-            if (left != nullptr)
-            {
-                result &= *left == *r.left;
-            }
-            if (right != nullptr)
-            {
-                result &= *right == *r.right;
-            }
-        }
-        return result;
+    int maxPathSum(TreeNode* root) {
+        getMax(root);
+        return ans;
     }
 };
 
-// Input: [1,2,3]
-
-//        1
-//       / \
-//      2   3
-
-// Output: 6
-
-// Input: [-10,9,20,null,null,15,7]
-
-//       -10 (34)
-//     /       \
-//   9(9)       20 (35) 42
-//          /       \
-//         15(15)    7 (7)
-
-// Output: 42
-
-int max_ = INT_MIN;
-
-int maxGain(TreeNode* root) {
-    if (root == nullptr) return 0;
-    auto ans = 0;
-
-    auto l_max = max(maxGain(root->left), 0);
-    auto r_max = max(maxGain(root->right), 0);
-
-    max_ = max(max_, root->val + l_max + r_max);
-
-    return root->val + max(l_max, r_max);
-}
-
-int maxPathSum(TreeNode* root) {
-    maxGain(root);
-    return max_;
-}
-
 int main()
 {
-    assert(maxPathSum(
-        new TreeNode(vector<int>{1,2,3}))
-        == 6);
-    assert(maxPathSum(
-        new TreeNode(vector<int>{-10,9,20,-1,-1,15,7}))
-        == 42);
+    auto input1 = vectorToTree(vector<int>{1,2,3});
+    assert(Solution().maxPathSum(input1)
+        == (6));
+    auto input2 = vectorToTree(vector<int>{-10,9,20,-777,-777,15,7});
+    assert(Solution().maxPathSum(input2)
+        == (42));
+
     return 0;
 }
