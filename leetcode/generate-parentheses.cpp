a19b6df7db1b3978dd46e5a506d8a5c5b61076ad
doum1004 <doum1004@gmail.com>
2020-01-18 00:32:13 +0900

    update generate-parentheses

--- leetcode/generate-parentheses.cpp
+++ leetcode/generate-parentheses.cpp
@@ -7,46 +7,44 @@
 
 using namespace std;
 
-// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+// https://leetcode.com/problems/generate-parentheses/
 
-// For example, given n = 3, a solution set is:
+// n = 1
+// o=0 c=0
+// (
+// o=1 c=0
+// ()
+// o=1 c=1
+// 
 
-// [
-//   "((()))",
-//   "(()())",
-//   "(())()",
-//   "()(())",
-//   "()()()"
-// ]
+// n = 2
+// o=0
+// (
+// o=1
+// o
+// (()), ()()
+
+// tiem: O(b^d), backtracking algorithms where b is the branching factor and d is the maximum depth of recursion.
+// Backtracking is characterized by a number of decisions b that can be made at each level of recursion. If you visualize the recursion tree, this is the number of children each internal node has. You can also think of b as standing for "base", which can help you remember that b is the base of the exponential.
+// If we can make b decisions at each level of recursion, and we expand the recursion tree to d levels (ie: each path has a length of d), then we get b^d nodes. Since backtracking is exhaustive and must visit each one of these nodes, the runtime is O(b^d).
 
 class Solution {
 public:
-
-    vector<string> generateParenthesis_bruteforce(int n) {
-        return vector<string>();
-    }
-
-    void generateParenthesis_backtracking(vector<string> &ans, string parenthessis, int open, int close, int n) {
-        if (parenthessis.size() == n * 2)
+    vector<string> ans;
+    void generateParenthesis(string s, int n, int o, int c) {
+        if (n == o && n == c)
         {
-            ans.push_back(parenthessis);
+            ans.push_back(s);
             return;
         }
         
-        if (open < n)
-            generateParenthesis_backtracking(ans, parenthessis + "(", open + 1, close, n);
-        if (close < open)
-            generateParenthesis_backtracking(ans, parenthessis + ")", open, close + 1, n);
+        if (o < n) generateParenthesis(s + "(", n, o + 1, c);
+        if (c < o) generateParenthesis(s + ")", n, o, c + 1);
     }
-
-    vector<string> generateParenthesis_backtracking(int n) {
-        vector<string> ans;
-        generateParenthesis_backtracking(ans, "", 0, 0, n);
-        return ans;
-    }
-
+    
     vector<string> generateParenthesis(int n) {
-        return generateParenthesis_backtracking(n);
+        generateParenthesis("", n, 0, 0);
+        return ans;
     }
 };
 
