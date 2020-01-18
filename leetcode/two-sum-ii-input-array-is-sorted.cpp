79f6310407a2880b35d9bc304cac8f10466b32dd
doum1004 <doum1004@gmail.com>
2020-01-18 14:55:37 +0900

    update two-sum-ii

--- leetcode/two-sum-ii-input-array-is-sorted.cpp
+++ leetcode/two-sum-ii-input-array-is-sorted.cpp
@@ -7,49 +7,70 @@ using namespace std;
 
 // https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
+// [2,7,11,15] target(9) 
+// map for answer
+// time: o(2n) -> o(n)
+// space: o(1)
+
+// [7] = 0
+// [2] = 1 ...
+// [-3] = 2
+// [-6] = 3
+// iterate nums and find answer index
+// 2(0) -> [2](1)
+
+//
+// sliding door (final)
+// time: o(n)
+// space: o(1)
+
+// [2,7,11,15]
+//  l .     r
+//  l + r = target done
+//  l + r > target --r
+//  else ++l
+
+
 class Solution {
 public:
-    vector<int> twoSum2(vector<int>& numbers, int target) {
-        vector<int> ans;
-        if (numbers.size() < 2) return ans;
-        
-        // store result of intersect with index
-        auto store_intersection = unordered_map<int, int>();
-        int i = 0;
-        for (auto n : numbers) {
-            store_intersection[target - n] = i++;
-        }
-
-        for (int i=0; i<numbers.size()-1; i++) {
-            auto it = store_intersection.find(numbers[i]);
-            if (it != store_intersection.end()) {
-                auto j = it->second;
-                ans.push_back(i < j ? i + 1 : j + 1);
-                ans.push_back(i < j ? j + 1 : i + 1);
-                break;
-            }
-        }
-
-        return ans;
-    }
-
-    vector<int> twoSum1(vector<int>& numbers, int target) {
-        if (numbers.size() < 2) return {};
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        if (numbers.size() < 1) return vector<int>();
+        if (numbers.size() == 1) return (numbers[0] == target) ? vector<int> {1} : vector<int>();
         
         int l = 0, r = numbers.size() - 1;
         while (l < r) {
             auto sum = numbers[l] + numbers[r];
-            if (sum == target) return {l + 1, r + 1};
-            else if (sum > target) r--;
-            else l++;
+            if (sum == target) {
+                return vector<int> {l + 1, r + 1};
+            }
+            else if (sum > target) {
+                --r;
+            }
+            else {
+                ++l;
+            }
         }
-
-        return {};
+        
+        return vector<int>();
     }
-
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        return twoSum1(numbers, target);
-        //return twoSum2(numbers, target);
+    
+    vector<int> twoSum2(vector<int>& numbers, int target) {
+        // diff map to store answer
+        auto diff = unordered_map<int, int>();
+        for (int i=0; i<numbers.size(); ++i) {
+            diff[target - numbers[i]] = i;
+        }
+        
+        vector<int> ans;
+        for (int i=0; i<numbers.size(); ++i) {
+            if (diff.find(numbers[i]) != diff.end()) {
+                auto j = diff[numbers[i]];
+                ans.push_back((i < j) ? i + 1 : j + 1);
+                ans.push_back((i < j) ? j + 1 : i + 1);
+                return ans;
+            }
+        }
+        return ans;
     }
 };
 
