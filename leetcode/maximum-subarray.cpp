6102d1a145e7bab8e782c72246459ef835234a34
doum1004 <doum1004@gmail.com>
2020-01-18 23:02:55 +0900

    update maximum-subarray

--- leetcode/maximum-subarray.cpp
+++ leetcode/maximum-subarray.cpp
@@ -7,35 +7,41 @@ using namespace std;
 
 // https://leetcode.com/problems/maximum-subarray/
 
+// sliding window
+// time: o(n)
+// maxs = num[0]
+// curs = num[0]
+// -2 1 -3 4 -1 2 1 -5 4
+// loop ...                 curs = max(num[i], curs + num[i]) maxs = max(curs, maxs)
+//    i                     curs = max(1, -2+1=-1)  maxs = max(-2, 1) = 1
+//       i                  curs = max(-3, 1-3 = -2)    maxs = (1,-2) = 1
+//         i                curs = max(4, -2+4=2) = 4   maxs = (1, 4) = 4
+// ...
+
+    
+
 class Solution {
 public:
-    int maxSubArray_greedy_dp(vector<int>& nums) {
-        if (nums.size() < 2) return (nums.size() == 0) ? 0 : nums[0];
-
-        auto maxSum = nums[0];
-        for (int i=1; i<nums.size(); ++i) {
-            if (nums[i - 1] > 0) nums[i] += nums[i - 1];
-            maxSum = max(maxSum, nums[i]);
-        }
-
-        return maxSum;
-    }
-
-    int maxSubArray_greedy(vector<int>& nums) {
-        if (nums.size() < 2) return nums.size() == 0 ? 0 : nums[0];
-        
-        auto maxSum = nums[0], curSum = nums[0];
-        for (int i=1; i<nums.size(); ++i) {
-            curSum = max(nums[i], curSum + nums[i]);
-            maxSum = max(maxSum, curSum);
-        }
-
-        return maxSum;
-    }
-
     int maxSubArray(vector<int>& nums) {
-        return maxSubArray_greedy_dp(nums);
-        //return maxSubArray_greedy(nums);
+        if (nums.size() == 1) return nums[0];
+        
+        int maxSum = nums[0];
+        if (false) {
+            int curSum = nums[0];
+            for (int i=1; i<nums.size(); ++i) {
+                curSum = max(nums[i], curSum + nums[i]);
+                maxSum = max(curSum, maxSum);
+            }   
+        }
+        else {
+            // DP. set cursum on previous
+            for (int i=1; i<nums.size(); ++i) {
+                nums[i] = max(nums[i], nums[i-1] + nums[i]);
+                maxSum = max(maxSum, nums[i]);
+            }
+        }
+        
+        return maxSum;
     }
 };
 
