57836f4c7ae2e51d38a131b1d0234f8bd1cbf3c7
doum1004 <doum1004@gmail.com>
2020-01-18 14:30:55 +0900

    update merge-intervals

--- leetcode/merge-intervals.cpp
+++ leetcode/merge-intervals.cpp
@@ -5,30 +5,38 @@
 
 using namespace std;
 
-// https://leetcode.com/problems/search-in-rotated-sorted-array/
+// https://leetcode.com/problems/merge-intervals/
+
+// sort and merge
+// [1,3], [2,6], [8,10], [15,18]
+// sort
+// [1,3], [2,6], [8,10], [15,18]
+// merge vector
+// v [1, 3]
+// v [1, 6]
+// v [1, 6] [8, 10]
+// v [1, 6] [8, 10] [15, 18]
 
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return intervals;
-
+        if (intervals.size() < 2) return intervals;
+        
         // sort
-        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
-            return a[0] < b[0];
+        sort(intervals.begin(), intervals.end(), [&](auto &a, auto &b){
+           return a[0] < b[0]; 
         });
-
-        vector<vector<int>> ans;
+        
         // merge
-        for (auto interval : intervals) {
+        vector<vector<int>> ans;
+        for (auto &interval : intervals) {
             if (ans.empty() || ans.back()[1] < interval[0]) {
                 ans.push_back(interval);
             }
             else {
-                auto back = ans.back();
-                ans.back()[1] = max(back[1], interval[1]);
+                ans.back()[1] = max(ans.back()[1], interval[1]);
             }
         }
-
         return ans;
     }
 };
