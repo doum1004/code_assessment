f58b1d219f5453fd03b6110708c4d306fe11bb41
doum1004 <doum1004@gmail.com>
2020-01-19 22:42:19 +0900

    update find-median-from-dta-stream

--- leetcode/find-median-from-data-stream.cpp
+++ leetcode/find-median-from-data-stream.cpp
@@ -9,6 +9,30 @@ using namespace std;
 
 // https://leetcode.com/problems/find-median-from-data-stream/
 
+// Solution1 simple sort at findMedian
+// time: o(nlogn). add: o(1). median: sort o(nlogn)
+// space: o(n) vector
+
+// Solution2 insertion sort
+// time: o(n). add: bin search(logn) insertion(n). median: o(1)
+// space: o(n)
+//auto it = lower_bound(v.begin(), v.end(), num);
+//v.insert(it, num)
+
+// Solution3 heaps. min-heap, max-heap
+// time: o(logn) heap delete/insert logn
+// priority_queue<int> lo; //max-heap
+// priority_queue<int, vector<int>, greater<int>> hi; //min-heap
+// add(num)
+//  lo.push(num)
+//  hi.push(num.top())
+//  lo.pop()
+//  if (lo.size() < hi.size())
+//      lo.push(hi.top())
+//      hi.pop()
+//  findMin()
+//      return (lo.size() > hi.size()) ? lo.top() : ((lo.top() + hi.top()) / 2.0)
+
 class MedianFinder {
 public:
     /** initialize your data structure here. */
@@ -18,23 +42,73 @@ public:
     
     void addNum(int num) {
         lo.push(num);
-        
         hi.push(lo.top());
         lo.pop();
-
-        if (hi.size() > lo.size()) {
+        
+        if (lo.size() < hi.size()) {
             lo.push(hi.top());
             hi.pop();
         }
     }
     
     double findMedian() {
-        return (lo.size() > hi.size()) ? (double) lo.top() : (lo.top() + hi.top()) / 2.0;
+        if (lo.size() == 0) return 0;
+        return (lo.size() > hi.size()) ? lo.top() : (lo.top() + hi.top()) / 2.0;
     }
-
+    
 private:
-    priority_queue<int> lo; //max-heap
-    priority_queue<int, vector<int>, greater<int>> hi;
+    priority_queue<int> lo; // max-heap
+    priority_queue<int, vector<int>, greater<int>> hi; // min-heap
+};
+
+class MedianFinder_insertion_sort {
+public:
+    /** initialize your data structure here. */
+    MedianFinder_insertion_sort() {
+        
+    }
+    
+    void addNum(int num) {
+        if (data_.size() == 0) {
+            data_.push_back(num);
+        }
+        else {
+            data_.insert(lower_bound(data_.begin(), data_.end(), num), num);
+        }
+    }
+    
+    double findMedian() {
+        int n = data_.size();
+        if (n == 0) return 0;
+        
+        return (n % 2 == 1) ? (double)data_[n / 2] : ((data_[n / 2 - 1] + data_[n / 2]) / 2.0);
+    }
+    
+private:
+    vector<int> data_;
+};
+
+class MedianFinder_simple_sort {
+public:
+    /** initialize your data structure here. */
+    MedianFinder_simple_sort() {
+        
+    }
+    
+    void addNum(int num) {
+        data_.push_back(num);
+    }
+    
+    double findMedian() {
+        int n = data_.size();
+        if (n == 0) return 0;
+        
+        sort(data_.begin(), data_.end());
+        return (n % 2 == 1) ? (double)data_[n / 2] : ((data_[n / 2 - 1] + data_[n / 2]) / 2.0);
+    }
+    
+private:
+    vector<int> data_;
 };
 
 /**
