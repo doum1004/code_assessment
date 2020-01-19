7821029cca0f8c916e3238719b22bbe87bd613e7
doum1004 <doum1004@gmail.com>
2020-01-20 01:01:34 +0900

    update reverse-interger

--- leetcode/reverse-integer.cpp
+++ leetcode/reverse-integer.cpp
@@ -8,20 +8,33 @@
 using namespace std;
 
 // https://leetcode.com/problems/reverse-integer/
+// 123 -> 321
+
+// x = 123
+// c = 123 % 10 = 3
+// ans = 3
+
+// x = 12 (123 / 10)
+// c = 12 % 10 = 2
+
+// if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && c > 7))
+// if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && c > 8))
+// ans = ans * 10 + c = 32
+
+// ...
+// ans = 321
 
 class Solution {
 public:
     int reverse(int x) {
         int ans = 0;
         while (x != 0) {
-            int pop = x % 10;
+            int c = x % 10;
             x /= 10;
-            
-            if (ans > INT_MAX / 10 || (ans == INT_MAX && pop == 7)) return 0;
-            if (ans < INT_MIN / 10 || (ans == INT_MIN && pop == -8)) return 0;
-            ans = ans * 10 + pop;
+            if (ans > INT_MAX / 10 || (ans == INT_MAX && c > 7)) return 0; // INT_MAX 2147483647
+            if (ans < INT_MIN / 10 || (ans == INT_MIN && c > 8)) return 0; // INT_MIN -2147483648
+            ans = ans * 10 + c;
         }
-
         return ans;
     }
 };
