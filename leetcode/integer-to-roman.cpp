f3a52f1a4b2b9e69ce393991854f8e6b0565b102
doum1004 <doum1004@gmail.com>
2020-01-20 23:05:50 +0900

    update integer-to-roman

--- leetcode/integer-to-roman.cpp
+++ leetcode/integer-to-roman.cpp
@@ -7,29 +7,47 @@ using namespace std;
 
 // https://leetcode.com/problems/integer-to-roman/
 
-// Symbol       Value
-// I             1
-// V             5
-// X             10
-// L             50
-// C             100
-// D             500
-// M             1000
+// subtract maximum possibility
+// o(n) : roman mapper(13 * max(4))
+// o(n) : map(roman mapper)
+
+// map<int, pair<string, int>>
+// [0] = {M, 1000}
+// [1] = {CM, 900}
+
+// ans = ""
+// for (i in map.size()-1)
+//  while (num - map[i].second > 0)
+//      ans += map[i].first
+//      n -= map[i].second
+// return ans
+
 
 class Solution {
 public:
     string intToRoman(int num) {
-        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        string symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        vector<pair<string, int>> romans;
+        romans.push_back({"M", 1000});
+        romans.push_back({"CM", 900});
+        romans.push_back({"D", 500});
+        romans.push_back({"CD", 400});
+        romans.push_back({"C", 100});
+        romans.push_back({"XC", 90});
+        romans.push_back({"L", 50});
+        romans.push_back({"XL", 40});
+        romans.push_back({"X", 10});
+        romans.push_back({"IX", 9});
+        romans.push_back({"V", 5});
+        romans.push_back({"IV", 4});
+        romans.push_back({"I", 1});
         
         string ans = "";
-        for (int i=0; i<13; i++) {
-            while (num - values[i] >= 0) {
-                num -= values[i];
-                ans += symbols[i];
+        for (auto v : romans) {
+            while (num - v.second >= 0) {
+                ans += v.first;
+                num -= v.second;
             }
         }
-        
         return ans;
     }
 };
