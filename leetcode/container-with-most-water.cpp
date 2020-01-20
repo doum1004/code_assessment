88435b969a71d6e4935c102fbd3e844e083873f3
doum1004 <doum1004@gmail.com>
2020-01-20 22:40:55 +0900

    update container-with-most-water

--- leetcode/container-with-most-water.cpp
+++ leetcode/container-with-most-water.cpp
@@ -7,24 +7,38 @@ using namespace std;
 
 // https://leetcode.com/problems/container-with-most-water/
 
+// 1 8 6 2 5 4 8 3 7
+// [1,8] 1*(1-0) = 1
+// [8, 7] 7(min(8,7))*7(8-1)
+// keep biger height and moves and store max
+
+// sliding windows
+// time: o(n)
+// space: o(1)
+// 1 8 6 2
+// l     r  maxArea = 3
+//   l   r  maxArea = 4
+//   l r    maxArea = 6
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        // two-points sliding window
-        // time: o(n)
-        // space: o(1)
-
-        int ans = 0;
-        int i = 0, j = height.size() - 1;
-        while (i < j) {
-            ans = max(ans, (j - i) * min(height[i], height[j]));
-            if (height[i] < height[j])
-                i++;
-            else
-                j--;
+        if (height.size() < 2) return 0;
+        
+        int maxArea = 0;
+        int l = 0, r = height.size() - 1;
+        while (l < r) {
+            auto h1 = height[l];
+            auto h2 = height[r];
+            maxArea = max(maxArea, (r-l) * min(h1, h2));
+            if (h1 > h2) {
+                r--;
+            }
+            else {
+                l++;
+            }
         }
-
-        return ans;
+        return maxArea;
     }
 };
 
