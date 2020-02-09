0f5d850c83b96d81ab96dc5f4648977c7b136ad6
doum1004 <doum1004@gmail.com>
2020-02-09 18:00:05 +0900

    update

--- leetcode/add-two-numbers.cpp
+++ leetcode/add-two-numbers.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
-#include "../utils.h"
+#include "../listnode.h"
 
 using namespace std;
-
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode(int v) : val(v), next(NULL) {}
-};
-
-bool isEqual(ListNode* l, ListNode* r) {
-    if (l != nullptr && r != nullptr) return (l->val == r->val && isEqual(l->next, r->next));
-    return (l == nullptr && r == nullptr) ? true : false;
-}
 
 // https://leetcode.com/problems/add-two-numbers/
 
