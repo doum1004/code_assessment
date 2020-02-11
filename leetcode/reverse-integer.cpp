d9d92ec109229cda944fc828d201092a88607605
doum1004 <doum1004@gmail.com>
2020-02-11 21:34:34 +0900

    update

--- leetcode/reverse-integer.cpp
+++ leetcode/reverse-integer.cpp
@@ -7,35 +7,46 @@
 
 using namespace std;
 
-// https://leetcode.com/problems/reverse-integer/
-// 123 -> 321
+/**
+https://leetcode.com/problems/reverse-integer/
 
-// x = 123
-// c = 123 % 10 = 3
-// ans = 3
+// solution1. pop and push digits & check overflow
+// time: o(log(n)) : log10(n) = n number of digit place
+// space: o(1)
 
-// x = 12 (123 / 10)
-// c = 12 % 10 = 2
+123 % 10 = 3 3
+12 % 10 = 2  32
+1 & 10 = 1   321
 
-// if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && c > 7))
-// if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && c > 8))
-// ans = ans * 10 + c = 32
+1. loop till input goes 0
+2. get mode of first digit
+2. remove first digit in input
+3. check the overflow
+4. add the mode in ans
 
-// ...
-// ans = 321
+int ans = 0;
+while (x!=0)
+    int c = x % 10;
+    x /= 10;
+    if (ans > INT_MAX/10 || (ans == INT_MAX/10 && c>7)) return 0
+    if (ans < INT_MIN/10 || (ans == INT_MIN/10 && c>8)) return 0
+    ans = c + ans*10;
+
+*/
 
 class Solution {
 public:
     int reverse(int x) {
         int ans = 0;
-        while (x != 0) {
-            int c = x % 10;
+        while (x!=0) {
+            int c = x%10;
             x /= 10;
-            if (ans > INT_MAX / 10 || (ans == INT_MAX && c > 7)) return 0; // INT_MAX 2147483647
-            if (ans < INT_MIN / 10 || (ans == INT_MIN && c > 8)) return 0; // INT_MIN -2147483648
-            ans = ans * 10 + c;
+            if (ans > INT_MAX / 10 || (ans == INT_MAX && c > 7)) return 0;
+            if (ans < INT_MIN / 10 || (ans == INT_MIN && c > 8)) return 0;
+            ans = ans*10 + c;
         }
         return ans;
+        
     }
 };
 
