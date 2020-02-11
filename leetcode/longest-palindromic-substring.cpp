13f5125a4951243e77308c9667358e0d0259ad23
doum1004 <doum1004@gmail.com>
2020-02-11 21:17:22 +0900

    update

--- leetcode/longest-palindromic-substring.cpp
+++ leetcode/longest-palindromic-substring.cpp
@@ -5,39 +5,51 @@
 
 using namespace std;
 
-// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
-// expand from center
-// expand from index to check palindromic
-// time: log (n^2) log(n)
-// space: n(1)
+/**
 
-// TODO
-// Longest Palindromic Substring Manacher's Algorithm
-// https://www.youtube.com/watch?v=V-sEwsca1ak
+// solution1. expand from center
+time: o(n^2) : iterate(n) expand(n)
+space: o(1)
+
+babad
+*         b
+ *        a
+  *       aba
+  l
+  r
+ l r
+   *.     a
+    *.    d
+    
+babbad
+  *       abba
+  lr
+ l  r
+
+// solution2. manacher's al
+
+*/
 
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int start=0, len = -1;
-        int n = s.size();
-        for (int i=0; i<n; ++i) {
-            auto l = i, r = i;
-            while (s[l] == s[r+1]) { // !! skip same char
-                r++;
-                i++;
-            }
-            while (l - 1 >= 0 && r + 1 < n && s[l-1] == s[r+1]) {
+        int len_palindorme = 0;
+        int start=-1;
+        for (int i=0; i<s.size(); ++i) {
+            int l = i;
+            while (i+1 < s.size() && s[i+1] == s[i]) i++;
+            int r = i;
+            while (l >= 0 && r<s.size() && s[l] == s[r]) {
+                if (len_palindorme < r-l+1) {
+                    len_palindorme = r-l+1;
+                    start = l;
+                }
                 l--;
                 r++;
             }
-            
-            if (r-l+1 > len) {
-                len = r-l+1;
-                start = l;
-            }
+            if (r+1 == s.size()) break;
         }
-        
-        return s.substr(start, len);
+        return (start >= 0) ? s.substr(start,len_palindorme) : "";
     }
 };
 
