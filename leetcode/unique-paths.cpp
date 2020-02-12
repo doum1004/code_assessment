4e3fec59c3f40892dd68ebe7d5929d935871df57
doum1004 <doum1004@gmail.com>
2020-02-13 00:58:30 +0900

    update

--- leetcode/unique-paths.cpp
+++ leetcode/unique-paths.cpp
@@ -5,124 +5,129 @@
 using namespace std;
 
 /**
-// Solution1. DSF preorder and DP (memoryzation)
+https://leetcode.com/problems/unique-paths/
+
+// Solution1. recursion DSF preorder with memorization
+// time: o(m*n) : visiting node(m*n)
+// space: o(m*n) : m*n(board) + m*n(recursion)
+
+1. create a board (m*n) vector<vector<int>> init(-1)
+2. recursion call. int recursion(board, 0, 0)
+3. in recursion, check input validation. invalid: return 0
+4. check it is in dp and return dp value or if it reaches end and return 1
+5. check bottom, right path and get result
+6. and save it in bp
+7. return total count of left, right
+
+
+// Solution2. iteration DP (full board)
 // time: o(m*n)
-// space: o(m+n) recursion
-1. init board row, col (-1)
-2. visit all path in DSF preorder. If cur node is final, return 1 otherwise return 0
-3. store the number of path for reuse later
+// space: o(m*n)
+1. create a full board (init 1)
+2. iterate all after first row and col
+3. update board. board[i][j] = board[i-1][j] + board[i][j-1]
+1 1 1 1
+1 2 3 4           
+1 3 6 10
 
+// Soluiton3. iteration dp two vector
+// time: o(m*n)
+// space: o(2*n)
+1. create two vectors with init(1)
+2. iterate after first row and col, cur[i] = cur[i-1] + pre[i]
+3. swap cur and pre
 
-//Solution2. Iteration DP
-//time: o(m*n)
-//space: o(m*n)
+1 1 1 1 pre
+1 2 3 4 cur
+. . . .
 
-dp table m*n
-1 1 1
-1 1 1
-1 1 1
+1 1 1 1 
+1 2 3 4 pre
+1 3 6 10 cur
 
-1 1 1
-1 2 3
-1 3 6
+1 1 1 1 
+1 2 3 4 cur
+1 3 6 10 pre
 
-output = path[m-1][n-1]
+// Soluiton4. iteration dp one vector
+// time: o(m*n)
+// space: o(n)
+1. create a vector init (0)
+2. iterate from second row and col
+3. cur[i] += cur[i-1]
 
-//Solution2_2. Iteration DP
-//time: o(m*n)
-//space: o(2m)
+1 1 1 1 cur
+. . . .
+. . . .
 
-use two m vector
-1 1 1
-1 1 1
-1 1 1
+1 1 1 1 
+1 2 3 4 cur
+. . . .
 
-1 1 1 pre
-1 2 3 cur
-1 1 1
-
-1 1 1
-1 2 3 pre
-1 3 6 cur
-
-1 1 1
-1 2 3
-1 3 6 pre
-
-
-//Solution2_3. Iteration DP
-//time: o(m*n)
-//space: o(m)
-
-use two m vector
-1 1 1
-1 1 1
-1 1 1
-
-1 1 1 
-1 2 3 cur
-1 1 1
-
-1 1 1
-1 2 3 
-1 3 6 cur (cur[j] += cur[j-1])
+1 1 1 1 
+1 2 3 4 
+1 3 6 10 cur
 
 */
 
+
 class Solution {
 public:
-    int recursion(vector<vector<int>> &board, int r, int c) {
-        if (r < 0 || r >= board.size() || c < 0 || c >= board[0].size()) return 0;
-        if (r + 1 == board.size() && c + 1 == board[0].size()) return 1;
-    
-        if (board[r][c] >= 0) return board[r][c];
-        auto bottom = recursion(board, r + 1, c);
-        auto right = recursion(board, r, c+1);
-        board[r][c] = bottom + right;
-        return board[r][c];
+    int uniquePaths_recursion(vector<vector<int>> &board, int m, int n) {
+        if (m < 0 || m >= board.size() || n < 0 || n >= board[0].size()) return 0;
+        if (board[m][n] > -1) return board[m][n];
+        if (m == board.size()-1 && n == board[0].size()-1) return 1;
+        
+        auto bottom = uniquePaths_recursion(board, m, n+1);
+        auto right = uniquePaths_recursion(board, m+1, n);
+        board[m][n] = bottom+right;
+        
+        return bottom + right;
     }
     
     int uniquePaths_recursion(int m, int n) {
-        vector<vector<int>> board(n, vector<int>(m, -1));
-        return recursion(board, 0, 0);
+        vector<vector<int>> board(m, vector<int>(n, -1));
+        return uniquePaths_recursion(board, 0, 0);
     }
     
-    int uniquePaths_iteration1(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 1));
-        for (int r=1; r<m; ++r) {
-            for (int c=1; c<n; ++c) {
-                dp[r][c] = dp[r-1][c] + dp[r][c-1];
+    int uniquePaths_iteration_dp_fullvector(int m, int n) {
+        vector<vector<int>> board(m, vector<int>(n, 1));
+        for (int i=1; i<m; ++i) {
+            for (int j=1; j<n; ++j) {
+                board[i][j] = board[i-1][j] + board[i][j-1];
             }
         }
-        return dp[m-1][n-1];
+        
+        return board[m-1][n-1];
     }
     
-    int uniquePaths_iteration2(int m, int n) {
+    int uniquePaths_iteration_dp_twovector(int m, int n) {
         vector<int> pre(n, 1), cur(n, 1);
-        for (int r=1; r<m; ++r) {
-            for (int c=1; c<n; ++c) {
-                cur[c] = pre[c] + cur[c-1];
+        for (int i=1; i<m; ++i) {
+            for (int j=1; j<n; ++j) {
+                cur[j] = cur[j-1] + pre[j];
             }
-            swap(pre, cur);
+            swap(cur, pre);
         }
+        
         return pre[n-1];
     }
     
-    int uniquePaths_iteration3(int m, int n) {
+    int uniquePaths_iteration_dp_onevector(int m, int n) {
         vector<int> cur(n, 1);
-        for (int r=1; r<m; ++r) {
-            for (int c=1; c<n; ++c) {
-                cur[c] += cur[c-1];
+        for (int i=1; i<m; ++i) {
+            for (int j=1; j<n; ++j) {
+                cur[j] += cur[j-1];
             }
         }
         return cur[n-1];
     }
     
     int uniquePaths(int m, int n) {
-        //return uniquePaths_recursion(m,n);
-        //return uniquePaths_iteration1(m,n);
-        //return uniquePaths_iteration2(m,n);
-        return uniquePaths_iteration3(m,n);
+        //return uniquePaths_recursion(m, n);
+        //return uniquePaths_iteration_dp_fullvector(m, n);
+        //return uniquePaths_iteration_dp_twovector(m, n);
+        return uniquePaths_iteration_dp_onevector(m, n);
     }
 };
 
