9be0575bdbca740bb5530f034ffae592077f8702
doum1004 <doum1004@gmail.com>
2020-02-13 02:11:36 +0900

    update

--- leetcode/unique-paths-ii.cpp
+++ leetcode/unique-paths-ii.cpp
@@ -7,59 +7,83 @@ using namespace std;
 /**
 https://leetcode.com/problems/unique-paths-ii/
 
-//Solution1. Recursion DSF preorder with DP
-//time: o(m*n)
-//space: o(m+n) recursion
+// solution1. recursion with memorization
+// time: o(n)
+// space: o(n) recursion
+1. iterate board to set 0 as -1 and set 1 as 0
+2. call int recursion(board, 0, 0)
+3. check range validation
+4. check dp(if it hits >= 0) then return
+5. if it hits end return 1
+6. check bottom, right in recursion and save it
+7. return total
 
-1. visit node by DSF preorder
-2. skip path of obstacles (1)
-2. store count of path for DP (negative)
-
-
-//Solution2. Itreation with DP
-//time: o(m*n)
-//space: o(1)
+// solution2. iteation with dp
+// time: o(n)
+// space: o(n) for unsigned int
+1. init dp
+2. iterate board
+2.1 if node gets 1 then save as 0
+2.2 if i==0 && j==0 up to board
+2.3 if i==0 || j==0 up to previous dp node
+2.4 otherwise dp[][] = dp[-1][] + dp[][-1]
 
 */
 
 class Solution {
 public:
-    int uniquePathsWithObstacles_recursion(vector<vector<int>>& obstacleGrid, int r, int c) {
-        if (r < 0 || c < 0 || r >= obstacleGrid.size() || c >= obstacleGrid[0].size()) return 0;
-        if (obstacleGrid[r][c] != 0) return (obstacleGrid[r][c] > 0) ? 0 : abs(obstacleGrid[r][c]);
-        if (r + 1 == obstacleGrid.size() && c + 1 == obstacleGrid[0].size()) return 1;
+    int uniquePathsWithObstacles_recursion(vector<vector<int>>& obstacleGrid, int m, int n) {
+        if (m < 0 || n < 0 || m >= obstacleGrid.size() || n >= obstacleGrid[0].size()) return 0;
+        if (obstacleGrid[m][n] > -1) return obstacleGrid[m][n];
+        if (m == obstacleGrid.size() - 1 && n == obstacleGrid[0].size() - 1) return 1;
         
-        auto bottom = uniquePathsWithObstacles_recursion(obstacleGrid, r + 1, c);
-        auto right = uniquePathsWithObstacles_recursion(obstacleGrid, r, c+1);
-        obstacleGrid[r][c] = (bottom + right == 0) ? 1 : (bottom + right) * -1;
-        return bottom + right;
+        auto adjacent1 = uniquePathsWithObstacles_recursion(obstacleGrid, m+1, n);
+        auto adjacent2 = uniquePathsWithObstacles_recursion(obstacleGrid, m, n+1);
+        obstacleGrid[m][n] = adjacent1+adjacent2;
+        return obstacleGrid[m][n];
+    }
+    
+    int uniquePathsWithObstacles_recursion(vector<vector<int>>& obstacleGrid) {
+        for (int i=0; i<obstacleGrid.size(); ++i) {
+            for (int j=0; j<obstacleGrid[0].size(); ++j) {
+                obstacleGrid[i][j] = obstacleGrid[i][j] == 0 ? -1 : 0;
+            }
+        }
+        return uniquePathsWithObstacles_recursion(obstacleGrid, 0, 0);
     }
     
     int uniquePathsWithObstacles_iteration(vector<vector<int>>& obstacleGrid) {
-        int n_row = obstacleGrid.size();
-        int n_col = obstacleGrid[0].size();
-        vector<vector<unsigned int>> dp(n_row, vector<unsigned int>(n_col, 0));
-            
-        for (int i=0; i<n_row; ++i) {
-            for (int j=0; j<n_col; ++j) {
+        int size1 = obstacleGrid.size();
+        int size2 = obstacleGrid[0].size();
+        vector<vector<unsigned int>> dp(size1, vector<unsigned int>(size2, 0));
+        
+        for (int i=0; i<size1; ++i) {
+            for (int j=0; j<size2; ++j) {
                 if (obstacleGrid[i][j] == 1) {
                     dp[i][j] = 0;
                 }
                 else if (i == 0 || j == 0) {
-                    if (i == 0 && j == 0) dp[i][j] = 1;
-                    else if (i == 0) dp[i][j] = dp[i][j-1];
-                    else if (j == 0) dp[i][j] = dp[i-1][j];
+                    if (i == 0 && j == 0) {
+                        dp[i][j] = 1;
+                    }
+                    else if (i==0) {
+                        dp[i][j] = dp[i][j-1];
+                    }
+                    else if (j==0) {
+                        dp[i][j] = dp[i-1][j];
+                    }
                 }
                 else {
                     dp[i][j] = dp[i-1][j] + dp[i][j-1];
                 }
             }
         }
-        return dp[n_row-1][n_col-1];
+        
+        return dp[size1-1][size2-1];
     }
     
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        //return uniquePathsWithObstacles_recursion(obstacleGrid, 0, 0);
+        //return uniquePathsWithObstacles_recursion(obstacleGrid);
         return uniquePathsWithObstacles_iteration(obstacleGrid);
     }
 };
