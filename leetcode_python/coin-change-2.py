c84f6c41c0dc2448e1e2af7f046804adcaddebf7
doum1004 <doum1004@gmail.com>
2020-03-01 13:08:51 +0900

    solve

---
+++ leetcode_python/coin-change-2.py
@@ -0,0 +1,15 @@
+"""
+https://leetcode.com/problems/coin-change-2/
+"""
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        #cur = [0] * (amount+1)
+        cur = [0 for i in range(amount+1)]
+        cur[0] = 1
+        #coins.sort()
+        for coin in coins:
+            for i in range(coin, amount+1):
+                cur[i] += cur[i-coin]
+                
+        return cur[-1]
+        
