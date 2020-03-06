e48d5daa11edfe599cc2015c2840be7807f01263
3dsk-jehyeoklee <doum1004@gmail.com>
2020-03-06 20:16:45 +0900

    solve

--- leetcode/leetcode_cpp/container-with-most-water.cpp
+++ leetcode/leetcode_cpp/container-with-most-water.cpp
@@ -1,46 +1,51 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
-#include "../utils.h"
 
 using namespace std;
 
-// https://leetcode.com/problems/container-with-most-water/
+/**
+https://leetcode.com/problems/container-with-most-water/submissions/
 
-// 1 8 6 2 5 4 8 3 7
-// [1,8] 1*(1-0) = 1
-// [8, 7] 7(min(8,7))*7(8-1)
-// keep biger height and moves and store max
+Solution1. Brute force...
+time: o(n^2)
+space: o(1)
 
-// sliding windows
-// time: o(n)
-// space: o(1)
-// 1 8 6 2
-// l     r  maxArea = 3
-//   l   r  maxArea = 4
-//   l r    maxArea = 6
+Solution2. Sliding window
+time: o(n)
+space: o(1)
+
+1. set l=0, r=size()-1
+2. loop (l<r)
+2.1 save container mount
+2.2 if h[l] < l++ else r--
+
+1 8 6 2
+l     r  maxArea = 3
+  l   r  maxArea = 4
+  l r    maxArea = 6
+
+*/
 
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        if (height.size() < 2) return 0;
+        if (height.size() < 1) return 0;
         
         int maxArea = 0;
-        int l = 0, r = height.size() - 1;
-        while (l < r) {
-            auto h1 = height[l];
-            auto h2 = height[r];
-            maxArea = max(maxArea, (r-l) * min(h1, h2));
-            if (h1 > h2) {
-                r--;
-            }
-            else {
-                l++;
-            }
+        int l = 0, r = height.size()-1;
+        while (l<r) {
+            auto area = min(height[l], height[r]) * (r-l);
+            
+            maxArea = max(maxArea, area);
+            if (height[l] < height[r]) l++;
+            else r--;
         }
+        
         return maxArea;
     }
 };
+
 
 int main()
 {
