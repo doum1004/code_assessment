4d22678a46f1ac4107504a20f22c076f6ec5c03b
3dsk-jehyeoklee <doum1004@gmail.com>
2020-03-08 19:20:31 +0900

    solve

---
+++ leetcode/leetcode_python/word-ladder-ii.py
@@ -0,0 +1,60 @@
+from typing import List
+import collections
+
+"""
+https://leetcode.com/problems/word-ladder-ii/
+"""
+
+class Solution:
+    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+        dictionary = dict.fromkeys(wordList, 1)
+        if endWord not in dictionary:
+            return []
+        
+        def buildLadders(word, children, ladder, res):
+            if word == endWord:
+                res.append(list(ladder))
+            else:
+                for child in children[word]:
+                    ladder.append(child)
+                    buildLadders(child, children, ladder, res)
+                    ladder.pop()
+        
+        word_current = collections.defaultdict(int)
+        word_next = collections.defaultdict(int)
+        
+        res = []
+        
+        word_current[beginWord] = 1
+        ladder = []
+        ladder.append(beginWord)
+        
+        children = collections.defaultdict(list)
+        while (True):
+            for word in word_current:
+                if word in dictionary:
+                    del dictionary[word]
+                
+            for parent in word_current:
+                word = parent
+                for i in range(0, len(word)):
+                    for c in 'abcdefghijklmnopqrstuvwxyz':
+                        word = word[:i] + c + word[i+1:]
+                        if word in dictionary:
+                            word_next[word] = 1
+                            children[parent].append(word)
+                        
+                    word = parent
+            if not len(word_next):
+                break
+                
+            if endWord in word_next:
+                buildLadders(beginWord, children, ladder, res)
+                break
+        
+            word_current = dict(word_next)
+            word_next.clear()
+            
+        return res
+        
+        
