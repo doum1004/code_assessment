b650cbc3e381a281b1a8d8cbc5ffeec3d5a725df
3dsk-jehyeoklee <doum1004@gmail.com>
2020-03-08 22:39:46 +0900

    solve

---
+++ leetcode/leetcode_python/integer-to-roman.py
@@ -0,0 +1,16 @@
+"""
+https://leetcode.com/problems/integer-to-roman/
+"""
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        nums = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
+        symboles = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
+        
+        res = ''
+        for i in range(len(nums)):
+            res += num // nums[i] * symboles[i]
+            num %= nums[i]
+            
+        return res
+        
