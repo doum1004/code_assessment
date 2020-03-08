c1860b715d00bc47e5ff1454e8864b6b7725bd2b
3dsk-jehyeoklee <doum1004@gmail.com>
2020-03-08 23:07:04 +0900

    solve

---
+++ leetcode/leetcode_python/roman-to-integer.py
@@ -0,0 +1,40 @@
+"""
+https://leetcode.com/problems/roman-to-integer/
+"""
+
+class Solution:
+    def romanToInt_1(self, s: str) -> int:
+        nums = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
+        symboles = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
+        
+        res = 0
+        for i in range(len(symboles)):
+            l = len(symboles[i])
+            while symboles[i] in s[:l]:
+                res += nums[i]
+                s = s[l:]
+        return res
+    
+    def romanToInt_2(self, s: str) -> int:
+        sToi = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        
+        s = s.replace('IV', 'IIII').replace('IX', 'VIIII')
+        s = s.replace('XL', 'XXXX').replace('XC', 'LXXXX')
+        s = s.replace('CD', 'CCCC').replace('CM', 'DCCCC')
+        
+        res = 0
+        for c in s:
+            res += sToi[c]
+        return res
+        
+    def romanToInt(self, s: str) -> int:
+        #return self.romanToInt_1(s)
+        return self.romanToInt_2(s)
