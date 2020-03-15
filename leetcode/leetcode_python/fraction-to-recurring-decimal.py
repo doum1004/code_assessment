f2d47e3e838ded194e5b06b0f5e752ca29e76743
3dsk-jehyeoklee <doum1004@gmail.com>
2020-03-16 02:55:28 +0900

    update

--- leetcode/leetcode_python/fraction-to-recurring-decimal.py
+++ leetcode/leetcode_python/fraction-to-recurring-decimal.py
@@ -3,11 +3,10 @@ https://leetcode.com/problems/fraction-to-recurring-decimal/
 """
 
 class Solution:
-    def fractionToDecimal_division(self, numerator: int, denominator: int) -> str:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
         if numerator == 0:
-            return '0'
-        if denominator == 0:
-            raise Exception('invalid input')
+            return "0"
+        if numerator == denominator: return "1"
         
         res = ''
         if (numerator < 0) ^ (denominator < 0):
@@ -15,26 +14,22 @@ class Solution:
         
         n = abs(numerator)
         d = abs(denominator)
-        res += str(n//d)
-        r = n % d
-        if r == 0:
+        res += str(n // d)
+        if n % d == 0:
             return res
         
-        res += '.'
-        m = {}
-        while r:
-            if r in m:
-                res = res[:m[r]] + '(' + res[m[r]:]
-                res += ')'
+        h = {}
+        
+        res += "."
+        n %= d
+        while n:
+            n *= 10
+            if n in h:
+                i = h[n]
+                res = res[:i] + "(" + res[i:] + ")"
                 break
-            m[r] = len(res)
-            r *= 10
-            res += str(r//d)
-            r %= d
+            h[n] = len(res)
+            res += str(n//d)
+            n %= d
         
         return res
-        
-        
-    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
-        return self.fractionToDecimal_division(numerator, denominator)
-        
