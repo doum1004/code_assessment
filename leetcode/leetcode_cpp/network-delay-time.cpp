ac024882426f5a491c58a9aac321736a0e09043c
3dsk-jehyeoklee <doum1004@gmail.com>
2020-03-22 02:12:41 +0900

    update

--- leetcode/leetcode_cpp/network-delay-time.cpp
+++ leetcode/leetcode_cpp/network-delay-time.cpp
@@ -18,17 +18,23 @@ space: o(E + N), E(all edges in graph), N(nodes in stack)
 2. start recursion from K (graph, map<int, int> receivedtime, node, delay). add delay.
 3. check receivedtime and return max delay
 
-solution 2. Dijkstra's algorithm (none negative)
+solution 2. BSF
+//time: o(E+ElogE+N^2). build graph(E edges) + sort(ElogE) + BSF(N^2. node)
+//space: o(E+N). graph(E) queue(N)
+
+1. build graph and sort for visiting small first
+2. bsf to visit node using queue<node, time>. and check visited node time is smaller. and mark min visited time.
+
+solution 3. Dijkstra's algorithm (none negative)
 https://www.youtube.com/watch?v=XB4MIexjvY0
-// time: o(n^n). graph(E). visit(n)* relaxation(n-1. update vertiex)
+https://www.hackerearth.com/practice/algorithms/graphs/shortest-path-algorithms/tutorial/
+// time: o(n^2). graph(E). visit(n)*relaxation(n-1. update vertiex)
 // space: o(N+E). graph(E). table(N)
 
-
-solution 3. Bellman Ford (negative, no cycle negative sum)
+solution 4. Bellman Ford (negative, no cycle negative sum)
 https://www.youtube.com/watch?v=FtN3BYH2Zes
-// time: o(n^n)
+// time: o(n^3)
 // space: o(n)
-
 
 */
 
@@ -45,7 +51,7 @@ public:
     }
     
     
-    int networkDelayTime_(vector<vector<int>>& times, int N, int K) {
+    int networkDelayTime_dsf(vector<vector<int>>& times, int N, int K) {
         unordered_map<int,vector<pair<int,int>>> graph;
         for (auto &time:times) {
             graph[time[0]].push_back({time[2], time[1]});
@@ -71,66 +77,112 @@ public:
         return maxSignal;
     }
     
-    int networkDelayTime_dijkstra(vector<vector<int>>& times, int N, int K) {
-        // build graph
-        vector<vector<pair<int,int>>> g(N+1);
-        for (auto &e:times) {
-            int u = e[0], v = e[1], w = e[2];
-            g[u].push_back({v, w});
-        }        
+    int networkDelayTime_bsf(vector<vector<int>>& times, int N, int K) {
+        // 1.
+        unordered_map<int,vector<pair<int,int>>> g;
+        for (auto &time:times) {
+            g[time[0]].push_back({time[2], time[1]});
+        }
         
-        // dijkstra
-        vector<int> table(N+1, -1);
+        for (auto &n:g) {
+            sort(n.second.begin(), n.second.end());
+        }
         
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> q;
-        q.push({0, K});
+        // 2.
+        unordered_map<int, int> visited;
+        for (int i=1; i<=N; ++i) {
+            visited[i] = INT_MAX;
+        }
+        
+        queue<pair<int,int>> q; // {node, time}
+        q.push({K, 0});
         
         while (!q.empty()) {
-            auto delay = q.top().first;
+            auto node = q.front().first;
+            auto time = q.front().second;
+            q.pop();
+            
+            if (visited[node] <= time) continue;
+            visited[node] = time;
+            
+            for (auto &adj:g[node]) {
+                q.push({adj.second, time+adj.first});
+            }
+        }
+        
+        int res = 0;
+        for (auto &n:visited) {
+            if (n.second == INT_MAX) return -1;
+            if (n.first == K) continue;
+            res = max(n.second, res);
+        }
+    
+        return res;
+    }
+
+    int networkDelayTime_dijkstra(vector<vector<int>>& times, int N, int K) {
+        // build graph
+        unordered_map<int, vector<pair<int,int>>> g;
+        for (auto &time:times) g[time[0]].push_back({time[2], time[1]});
+        
+        // time table
+        vector<int> t(N+1, -1);
+        
+        // next node selection pool
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<void>> q;
+        q.push({0, K});
+        
+        // visit and update time table
+        while (!q.empty()) {
+            auto time = q.top().first;
             auto node = q.top().second;
             q.pop();
             
-            if (table[node] != -1) continue;
-            table[node] = delay;
+            if (t[node] != -1 && t[node] <= time) continue;
+            t[node] = time;
             
             for (auto &adj:g[node]) {
-                q.push({adj.second+delay, adj.first});
+                q.push({time+adj.first, adj.second});
             }
         }
         
-        // get maxDelay
-        int maxDelay = 0;
+        // take final time
+        int res = 0;
         for (int i=1; i<=N; ++i) {
-            if (table[i] == -1) return -1;
-            maxDelay = max(maxDelay, table[i]);
+            if (t[i] == -1) return -1;
+            res = max(res, t[i]);
         }
-        
-        return maxDelay;
+        return res;
     }
     
     int networkDelayTime_bellmanford(vector<vector<int>>& times, int N, int K) {
+        // build and update table
         vector<int> dist(N+1, INT_MAX);
         dist[K] = 0;
-        for (int i=0; i<=N; ++i) {
-            for (auto &e:times) {
-                auto u=e[0], v=e[1], w=e[2];
+        for (int i=0; i<N-1; ++i) {
+            bool changed = false;
+            for (auto &time:times) {
+                auto u=time[0], v=time[1], w=time[2];
                 if (dist[u] != INT_MAX && dist[v] > dist[u] + w) {
                     dist[v] = dist[u] + w;
+                    changed = true;
                 }
             }
+            if (!changed) break;
         }
         
-        int maxDelay = 0;
+        // find max
+        int res = 0;
         for (int i=1; i<=N; ++i) {
             if (dist[i] == INT_MAX) return -1;
-            maxDelay = max(maxDelay, dist[i]);
+            res = max(res, dist[i]);
         }
-        
-        return maxDelay;
+        return res;
     }
     
     int networkDelayTime(vector<vector<int>>& times, int N, int K) {
         //return networkDelayTime_dsf(times, N, K);
+        //return networkDelayTime_bsf(times, N, K);
         //return networkDelayTime_dijkstra(times, N, K);
         return networkDelayTime_bellmanford(times, N, K);
     }
