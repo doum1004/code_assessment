4ab9c66327b5b28f85bc24b65c08dd2f089c1906
3dsk-jehyeoklee <doum1004@gmail.com>
2020-04-02 22:05:32 +0900

    update

--- leetcode/leetcode_cpp/maximum-product-subarray.cpp
+++ leetcode/leetcode_cpp/maximum-product-subarray.cpp
@@ -24,8 +24,8 @@ public:
         if (nums.size() == 0) return INT_MIN;
         
         int preA, preB, res;
-        preA = preB = res = nums[nums.size()-1];
-        for (int i=nums.size()-2; i>=0; --i) {
+        preA = preB = res = nums[0];
+        for (int i=1; i<nums.size(); ++i) {
             auto a = nums[i];
             auto b = a * preA;
             auto c = a * preB;
@@ -34,6 +34,17 @@ public:
             
             res = max(res, max(preA, preB));
         }
+        
+//         preA = preB = res = nums[nums.size()-1];
+//         for (int i=nums.size()-2; i>=0; --i) {
+//             auto a = nums[i];
+//             auto b = a * preA;
+//             auto c = a * preB;
+//             preA = min(min(a,b),c);
+//             preB = max(max(a,b),c);
+            
+//             res = max(res, max(preA, preB));
+//         }
         return res;
     }
 };
