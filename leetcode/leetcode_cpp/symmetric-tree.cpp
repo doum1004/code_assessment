52a29e127f1e00e580b9431829c7f3b5526c286c
doum1004 <41279031+doum1004@users.noreply.github.com>
2020-07-26 20:34:27 +0900

    Update symmetric-tree.cpp

--- leetcode/leetcode_cpp/symmetric-tree.cpp
+++ leetcode/leetcode_cpp/symmetric-tree.cpp
@@ -61,42 +61,37 @@ return true
 
 class Solution {
 public:
-    bool isSymeetric_recursion(TreeNode* n1, TreeNode* n2) {
-        if (n1 == nullptr && n2 == nullptr) return true;
-        if (n1 == nullptr || n2 == nullptr) return false;
-
-        if (!isSymeetric_recursion(n1->left, n2->right)) return false;
-        if (n1->val != n2->val) return false;
-        if (!isSymeetric_recursion(n1->right, n2->left)) return false;
-
-        return true;
+    bool compareDFS(TreeNode* l, TreeNode* r) {
+        // o(n) o(n)
+        if (l == nullptr && r == nullptr) return true;
+        if (!l || !r || l->val != r->val) return false;
+        
+        return (compareDFS(l->left, r->right) && compareDFS(l->right, r->left));
     }
     
-    bool isSymeetric_iterative(TreeNode* root) {
-        stack<TreeNode*> s;
-        s.push(root);
-        s.push(root);
-        while (!s.empty()) {
-            auto n1 = s.top();
-            s.pop();
-            auto n2 = s.top();
-            s.pop();
-
-            if (n1 == nullptr && n2 == nullptr) continue;
-            if (n1 == nullptr || n2 == nullptr) return false;
-            if (n1->val != n2->val) return false;
-
-            s.push(n1->left);
-            s.push(n2->right);
-            s.push(n1->right);
-            s.push(n2->left);
-        }
-
+    bool compareBFS(TreeNode* root) {
+        // o(n) o(n)
+        if (root == nullptr) return true;
+        
+        queue<pair<TreeNode*, TreeNode*>> q;
+        q.push({root->left, root->right});
+        
+        while (!q.empty()) {
+            auto l = q.front().first;
+            auto r = q.front().second;
+            q.pop();
+            if (l == nullptr && r == nullptr) continue;
+            if (!l || !r || l->val != r->val) return false;
+            
+            q.push({l->left, r->right});
+            q.push({l->right, r->left});
+        }   
         return true;
     }
     
     bool isSymmetric(TreeNode* root) {
-        return isSymeetric_recursion(root, root);
+        return compareDFS(root, root);
+        //return compareBFS(root);
     }
 };
 
