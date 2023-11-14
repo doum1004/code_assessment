fdd3870409477d6488f9f5b65c87da5530ead29f
doum1004 <41279031+doum1004@users.noreply.github.com>
2023-11-13 22:43:37 -0500

    add valid-palindrome solution (#1)

---
+++ leetcode/leetcode_python/valid-palindrome.py
@@ -0,0 +1,27 @@
+
+"""
+https://leetcode.com/problems/valid-palindrome/
+"""
+
+class Solution:
+    def isPalindrome_1(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            print(i, j)
+            if not s[i].isalnum():
+                i += 1
+                continue
+            if not s[j].isalnum():
+                j -= 1
+                continue
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True
+        
+
+    def isPalindrome(self, s: str) -> bool:
+        s = [char.lower() for char in s if char.isalnum()]
+        return s == s[::-1]
