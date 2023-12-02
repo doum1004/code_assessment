64363633c5c30154b2d79aa928c76a216cac7432
JEHYEOK LEE <doum1004@gmail.com>
2023-12-01 23:46:00 -0500

    update evaluate-division

--- leetcode/leetcode_cpp/evaluate-division.cpp
+++ leetcode/leetcode_cpp/evaluate-division.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 /**
-https://leetcode.com/problems/evaluate-division/
+https://leetcode.com/problems/evaluate-division
 
 Solution1. Build Graph and DFS to find acuumulate factor
 time: o(e*q). build graph(e. equation) + (dfs(n) * queries(q))
@@ -57,9 +57,50 @@ public:
         return res;
     }
     
+    double dfs(unordered_map<string, vector<pair<string,double>>>& graph, string start, string end, double product, unordered_set<string>& visited) {
+        if (visited.count(start)) return -1;
+        if (!graph.count(start) || !graph.count(end)) return -1;
+        if (start == end) {
+            return product;
+        }
+
+        visited.insert(start);
+        for (auto& g : graph[start]) {
+            auto ans = dfs(graph, g.first, end, product * g.second, visited);
+            if (ans != -1)
+                return ans;
+        }
+        visited.erase(start);
+        return -1;
+    }
+
+    vector<double> calcEquation_1(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        int n = equations.size();
+        int m = queries.size();
+        if (m == 0) return {};
+        unordered_map<string, vector<pair<string,double>>> graph;
+        for (int i=0; i<n; ++i) {
+            auto& equation = equations[i];
+            auto& e1 = equation[0];
+            auto& e2 = equation[1];
+            auto& v = values[i];
+            graph[e1].push_back({e2, v});
+            graph[e2].push_back({e1, 1.0/v});
+        }
+        
+        vector<double> res;
+        for (auto& query : queries) {
+            auto& q1 = query[0];
+            auto& q2 = query[1];
+            unordered_set<string> visited;
+            res.push_back(dfs(graph, q1, q2, 1, visited));            
+        }
+        return res;
+    }
     
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        return calcEquation_graph_dfs(equations, values, queries);
+        //return calcEquation_graph_dfs(equations, values, queries);
+        return calcEquation_1(equations, values, queries);
     }
 };
 
--- leetcode/leetcode_py/evaluate-division.py
+++ leetcode/leetcode_py/evaluate-division.py
@@ -4,10 +4,13 @@ import collections
 
 """
 https://leetcode.com/problems/evaluate-division/
+Solution1. build graph
+time: o(e*q) equation * query graph
+space: o(q) query graph
 """
 
 class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+    def calcEquation_1(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         g = collections.defaultdict(dict)
         for i in range(len(equations)):
             e = equations[i]
@@ -37,3 +40,31 @@ class Solution:
             
         return res
         
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = defaultdict(list)
+        def dfs(start, end, product, visited):
+            if start in visited: return -1
+            if not start in graph or not end in graph: return -1
+            if start == end: return product
+            
+            visited.add(start)
+            for node in graph[start]:
+                ans = dfs(node[0], end, product*node[1], visited)
+                if ans != -1:
+                    return ans
+            visited.remove(start)
+            return -1
+        
+        for i, e in enumerate(equations):
+            e1 = e[0]
+            e2 = e[1]
+            v = values[i]
+            graph[e1].append([e2, v])
+            graph[e2].append([e1, 1.0/v])
+
+        res = []
+        for query in queries:
+            visited = set()
+            res.append(dfs(query[0], query[1], 1, visited))
+
+        return res
