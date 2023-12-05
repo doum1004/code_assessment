0e4c24bee16da2e8bffb6c0cabe39fd22a49030d
JEHYEOK LEE <doum1004@gmail.com>
2023-12-04 23:04:25 -0500

    update reverse-words-in-a-string

--- leetcode/leetcode_cpp/reverse-words-in-a-string.cpp
+++ leetcode/leetcode_cpp/reverse-words-in-a-string.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 /**
-https://leetcode.com/problems/reverse-words-in-a-string/
+https://leetcode.com/problems/reverse-words-in-a-string
 
 // Solution1. iterate from begin to end
 //time: o(n).
@@ -73,11 +73,67 @@ public:
         reverse(s.begin(), s.end());
         return s;
     }
+
+    string reverseWords_1(string& s) {
+        string res;
+        string word;
+        for (int i=0; i<=s.size(); ++i) {
+            if (i == s.size() || isspace(s[i])) {
+                if (word.empty()) continue;
+
+                if (res.empty()) res = word;
+                else res = word + " " + res;
+                word = "";
+            } else {
+                word += s[i];
+            }
+        }
+        return res;
+    }
+
+    string reverseWords_3_1(string& s) {
+        int n=s.size();
+        int l=n, r=n;
+        while (l >= 0) {
+            while (r > 0 && isspace(s[r-1])) r--;
+            l = r-1;
+            while (l > 0 && !isspace(s[l-1])) l--;
+            if (l < 0) break;
+
+            if (s.size() != n)
+                s += " ";
+            s += s.substr(l, r-l);
+
+            r = l-1;
+        }
+        s.erase(s.begin(), s.begin()+n);
+        return s;
+    }
+
+    string reverseWords_3(string& s) {
+        reverse(s.begin(), s.end());
+        int n = s.size();
+        int i = 0, l = 0, r = 0;
+        while (i < n) {
+            while (i < n && s[i] == ' ')
+                s[r++] = s[i++];
+
+            if (l < r) {
+                reverse(s.begin()+l, s.end()+r);
+                s[r++] = ' ';
+                l = r;
+            }
+            i++;
+        }
+        while (r > 0 && s[r-1] == ' ') r--;
+        s.resize(r);
+        return s;
+    }
     
     string reverseWords(string s) {
         //return reverseWords_iterate(s);
         //return reverseWords_istringstream(s);
-        return reverseWords_inplace(s);
+        return reverseWords_3(s);
     }
 };
 
--- leetcode/leetcode_py/reverse-words-in-a-string.py
+++ leetcode/leetcode_py/reverse-words-in-a-string.py
@@ -24,6 +24,41 @@ class Solution:
         if res: res = res[:-1]
         return res
         
+    def reverseWords_1(self, s: str) -> str:
+        res, word = '', ''
+        for i, c in enumerate(s):
+            if c != ' ':
+                word += c           
+            elif word:
+                if res: res = word + " " + res
+                else: res = word
+                word = ''     
+        if word:            
+            if res: res = word + " " + res
+            else: res = word
+
+        return res
+
+    def reverseWords_3(self, s: str) -> str:
+        s = s[::-1]
+        print(s)
+        n = len(s)
+        i, l, r = 0, 0, 0
+        while i < n:
+            while i < n and s[i] != ' ':
+                s = s[:r] + s[i] + s[r+1:]
+                r += 1
+                i += 1
+            
+            if l < r:
+                s = s[:l] + s[l:r][::-1] + ' ' + s[r+1:]
+                r += 1
+                l = r
+            i += 1
+        
+        while r > 0 and s[r-1] == ' ': r -= 1
+        return s[:r]
+
     def reverseWords(self, s: str) -> str:
         #return self.reverseWords_builtin(s)
         #return self.reverseWords_builtin2(s)
