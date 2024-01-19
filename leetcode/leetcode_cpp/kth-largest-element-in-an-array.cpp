adb5d306bc9e216a17ba386ffd57e2a63a3190c6
JEHYEOK LEE <doum1004@gmail.com>
2024-01-18 23:25:47 -0500

    edit kth-largest-element-in-an-array

--- leetcode/leetcode_cpp/kth-largest-element-in-an-array.cpp
+++ leetcode/leetcode_cpp/kth-largest-element-in-an-array.cpp
@@ -1,163 +1,77 @@
 /*
 https://leetcode.com/problems/kth-largest-element-in-an-array
 
-solution1. sort (sort,  nth_element, partial_sort)
+1. sort, nth_element, partial sort
 time: o(nlogn)
 space: o(1)
 
-solution2. heap (min/max heap, multiset)
+2. priority_queue
+time: o(nlogn)
+space: o(n)
 
-solution3. quickselection
-time: o(n^2). worst: o(n^2) best: o(n)
+3. quick selection
+https://www.youtube.com/watch?v=XEmy13g1Qxc
+time: o(n^2) best o(n)
+space: o(n)
+
 
 */
-
-
-class Solution {
-public:
-    int findKthLargest_1_1(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size() - k];
-    }
-
-    int findKthLargest_1_2(vector<int>& nums, int k) {
-        nth_element(nums.begin(), nums.begin() + k - 1, nums.end(), greater<int>());
-        return nums[k - 1];
-    }
-
-    int findKthLargest_1_3(vector<int>& nums, int k) {
-        partial_sort(nums.begin(), nums.begin() + k, nums.end(), greater<int>());
-        return nums[k - 1];
-    }
-
-    int findKthLargest_2(vector<int>& nums, int k) {
-        auto minHeap = priority_queue<int, vector<int>, greater<int>()>();
-        for (int n : nums) {
-            minHeap.push(n);
-            if (minHeap.size() > k) minHeap.pop();
-        }
-        return minHeap.top();
-    }
-
-    int findKthLargest_2_1(vector<int>& nums, int k) {
-        auto minHeap = multiset<int>();
-        for (int n : nums) {
-            minHeap.insert(n);
-            if (minHeap.size() > k) minHeap.erase(minHeap.begin());
-        }
-        return *minHeap.begin();
-    }
-
-    int findKthLargest_3(vector<int>& nums, int k) {
-        return quickSelect(nums, 0, nums.size() - 1, k);
-    }
-
-    int quickSelect(vector<int>& nums, int l, int r, int k) {
-        int tar = nums[l];
-        int i = l;
-        int j = r;
-        while (l < r) {
-            while (i<j && nums[j] >= tar)
-                j--;
-            if (i<j)
-                nums[i++] = nums[j]
-            while (i<j && nums[i] <= tar)
-                i++;
-            if (i<j)
-                nums[j--] = nums[i];
-        }
-        nums[i] = tar;
-        if (i == k)
-            return tar;
-        if (i > k)
-            quickSelect(nums, l, i-1, k);
-        else
-            quickSelect(nums, i+1, j, k);
-        
-    }
-
-    int findKthLargest(vector<int>& nums, int k) {
-        return findKthLargest_1_1(nums, k);
-    }
-};
-
-
 class Solution {
 public:
     int findKthLargest_1(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        return nums[k-1];
+        return nums[nums.size() - k];
     }
-
     int findKthLargest_1_2(vector<int>& nums, int k) {
         nth_element(nums.begin(), nums.begin() + k - 1, nums.end(), greater<int>());
-        return nums[k-1];
+        return nums[k - 1];
     }
-
-    int findKthLargest_2(vector<int>& nums, int k) {
-        priority_queue<int> q;
-        for (auto num : nums)
-            q.push(num);
-        while (k-1) {
-            q.pop();
-            k--;
+    int findKthLargest_1_3(vector<int>& nums, int k) {
+        partial_sort(nums.begin(), nums.begin() + k, nums.end(), greater<int>());
+        return nums[k - 1];
+    }
+    int findKthLargest_2_1(vector<int>& nums, int k) {
+        // max heap
+        priority_queue<int> pq;
+        for (int num : nums) pq.push(num);
+        while (k > 1) {
+            k -= 1;
+            pq.pop();
         }
-        return q.top();
+        return pq.top();
     }
-
-    int quickSelect(vector<int>& nums, int l, int r, int k) {
-        int tar = nums[l];
-        int i = l;
-        int j = r;
-        while (i < j) {
-            while (i < j && nums[j] >= tar)
-                j--;
-            if (i < j)
-                nums[i++] = nums[j];
-            while (i < j && nums[i] <= tar)
-                i++;
-            if (i < j)
-                nums[j--] = nums[i];
+    int findKthLargest_2_2(vector<int>& nums, int k) {
+        // min heap
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int num : nums) {
+            pq.push(num);
+            if (pq.size() > k) pq.pop();
         }
-        nums[i] = tar;
-        if (i == k)
-            return tar;
-        if (k > k)
-            return quickSelect(nums, l, i - 1, k);
-        else
-            return quickSelect(nums, i+1, r, k);
+        return pq.top();
     }
-
-    int findKthLargest(vector<int>& nums, int k) {
+    int findKthLargest_3(vector<int>& nums, int k) {
         int l = 0, r = nums.size() - 1;
-        while (l<=r) {
+        while (l <= r) {
             auto pivot = partition(nums, l, r);
-            if (pivot == k - 1) {
-                return nums[pivot];
-            }
-            else if (pivot > k - 1) {
-                r = pivot - 1;
-            }
-            else {
-                l = pivot + 1;
-            }
+            if (pivot == k - 1) return nums[pivot];
+            else if (pivot > k - 1) r = pivot - 1;
+            else l = pivot + 1;
         }
-        
-        return INT_MIN;
+        return 0;
     }
-
     int partition(vector<int>& nums, int l, int r) {
         if (l == r) return l;
         int idx = l;
-        int randpivot = rand() % (r - l) + l;
+        // rand pivot (optional)
+        int randpivot = rand() % (r-l) + l;
         swap(nums[randpivot], nums[r]);
-        
         for (int i=l; i<r; ++i) {
-            if (nums[i] > nums[r]) {
-                swap(nums[i], nums[idx++]);
-            }
+            if (nums[i] > nums[r]) swap(nums[i], nums[idx++]);
         }
         swap(nums[idx], nums[r]);
         return idx;
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        return findKthLargest_3(nums, k);        
     }
 };
--- leetcode/leetcode_py/kth-largest-element-in-an-array.py
+++ leetcode/leetcode_py/kth-largest-element-in-an-array.py
@@ -1,20 +1,55 @@
+'''
+https://leetcode.com/problems/kth-largest-element-in-an-array
+
+1. sort, nth_element, partial sort
+time: o(nlogn)
+space: o(1)
+
+2. priority_queue
+time: o(nlogn)
+space: o(n)
+
+3. quick selection
+https://www.youtube.com/watch?v=XEmy13g1Qxc
+time: o(n^2) best o(n)
+space: o(n)
+'''
 class Solution:
     def findKthLargest_1(self, nums: List[int], k: int) -> int:
         nums.sort()
-        return nums[-k]
-    
+        return nums[len(nums)-k]
+
     def findKthLargest_2(self, nums: List[int], k: int) -> int:
-        # https://leetcode.com/problems/kth-largest-element-in-an-array/solutions/3906260/100-3-approaches-video-heap-quickselect-sorting/?envType=study-plan-v2&envId=top-interview-150
-        heap = nums[:k]
-        heapq.heapify(heap)
-        
-        for num in nums[k:]:
-            if num > heap[0]:
+        # min heap
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+            if len(heap) > k:
                 heapq.heappop(heap)
-                heapq.heappush(heap, num)
-        
         return heap[0]
 
+    def findKthLargest_3(self, nums: List[int], k: int) -> int:
+        l, r = 0, len(nums)-1
+        def partition(l1, r1):
+            if l1 == r1:
+                return l1
+            idx = l1
+            for i in range(l1, r1):
+                if nums[i] > nums[r1]:
+                    nums[i], nums[idx] = nums[idx], nums[i]
+                    idx += 1
+            nums[idx], nums[r1] = nums[r1], nums[idx]
+            return idx
+        while l <= r:
+            pivot = partition(l, r)
+            if pivot == k - 1:
+                return nums[pivot]
+            elif pivot < k - 1:
+                l = pivot + 1
+            else:
+                r = pivot - 1
+        return -1
+        
     def findKthLargest(self, nums: List[int], k: int) -> int:
         return self.findKthLargest_2(nums, k)
         
